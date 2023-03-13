08fd32e2f905ff5c859fb71a93d2cc96cc148b25
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2023-03-13 08:42:24 +0100

    Added dialog screen for email

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
-import assert from "assert";
-import { setRef, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Stack } from "@mui/system";
+import assert from "assert";
 import { Poppins } from "next/font/google";
 import Head from "next/head";
+import { useState } from "react";
 import {
   BsEmojiAngryFill,
   BsEmojiFrownFill,
@@ -13,13 +24,7 @@ import {
 const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
 import { initializeApp } from "firebase/app";
-import {
-  addDoc,
-  collection,
-  DocumentData,
-  getDocs,
-  getFirestore,
-} from "firebase/firestore";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.APIKEY,
@@ -42,22 +47,45 @@ enum Location {
   Bugibba = "Bugibba",
 }
 
-let selectedLocation: Location = Location.Burmarrad;
-
-const submitReview = async (rating: number, location: Location) => {
-  assert(rating >= 1 && rating <= 5, "Rating must be between 1 and 5");
-  try {
-    const docRef = await addDoc(collection(db, "Reviews"), {
-      date_created: new Date(),
-      rating: rating,
-      location: location,
-    });
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-};
-
 export default function Home() {
+  const [faceMenu, setFaceMenu] = useState(false);
+  const [ctaMenu, setCtaMenu] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState("");
+
+  let selectedLocation: Location = Location.Burmarrad;
+
+  const submitReview = async (rating: number, location: Location) => {
+    assert(rating >= 1 && rating <= 5, "Rating must be between 1 and 5");
+    try {
+      const docRef = await addDoc(collection(db, "Reviews"), {
+        date_created: new Date(),
+        rating: rating,
+        location: location,
+      });
+      setFaceMenu(false);
+      setCtaMenu(true);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  const submitEmail = async (email: string, location: Location) => {
+    try {
+      const docRef = await addDoc(collection(db, "Emails"), {
+        date_created: new Date(),
+        email: email,
+        location: location,
+      });
+      setCtaMenu(false);
+      setFaceMenu(true);
+      setOpen(false);
+      setEmail("");
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -67,7 +95,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="mainContainer">
+        <Box
+          justifyContent={"center"}
+          alignItems={"center"}
+          alignContent={"center"}
+          position={"absolute"}
+          display={"flex"}
+          height={"100%"}
+          width={"100%"}
+          flexDirection={"column"}
+          className={faceMenu ? "fadeIn" : "fadeOut"}
+        >
           <Typography
             textAlign={"center"}
             fontFamily={poppins.style.fontFamily}
@@ -81,31 +119,48 @@ export default function Home() {
             justifyContent={"space-around"}
             direction={"row"}
             width={"80%"}
+            marginTop={"10px"}
           >
             <BsEmojiAngryFill
               size={"80%"}
               cursor={"pointer"}
+              stroke={"#000000"}
+              strokeWidth={"0.2"}
+              color={"#FF0000"}
               onClick={() => submitReview(1, selectedLocation)}
             />
             <BsEmojiFrownFill
               size={"80%"}
               cursor={"pointer"}
+              color={"#FFA500"}
               onClick={() => submitReview(2, selectedLocation)}
+              strokeWidth={"0.2"}
+              stroke={"#000000"}
             />
             <BsEmojiNeutralFill
               size={"80%"}
+              color={"#FFFF00"}
+              stroke={"#000000"}
+              strokeWidth={"0.2"}
               cursor={"pointer"}
+              style={{ margin: "10px" }}
               onClick={() => submitReview(3, selectedLocation)}
             />
             <BsEmojiSmileFill
+              strokeWidth={"0.2"}
               size={"80%"}
               cursor={"pointer"}
+              stroke={"#000000"}
+              color={"#00FF00"}
               onClick={() => submitReview(4, selectedLocation)}
             />
             <BsFillEmojiHeartEyesFill
               size={"80%"}
+              color={"#00FFFF"}
               cursor={"pointer"}
+              stroke={"#000000"}
               onClick={() => submitReview(5, selectedLocation)}
+              strokeWidth={"0.2"}
             />
           </Stack>
 
@@ -124,7 +179,104 @@ export default function Home() {
               );
             })}
           </select>
-        </div>
+        </Box>
+        <Box
+          justifyContent={"center"}
+          alignItems={"center"}
+          alignContent={"center"}
+          position={"absolute"}
+          display={"flex"}
+          height={"100%"}
+          width={"100%"}
+          flexDirection={"column"}
+          className={ctaMenu ? "fadeIn" : "fadeOut"}
+          sx={{ transition: "opacity 1s ease-in-out" }}
+        >
+          <Typography
+            textAlign={"center"}
+            fontFamily={poppins.style.fontFamily}
+            fontSize={"3rem"}
+          >
+            Thank you for your feedback!
+          </Typography>
+          <Typography fontFamily={poppins.style.fontFamily} fontSize={"1.5rem"}>
+            Would you like to leave your email so we can contact you?
+          </Typography>
+
+          <Stack
+            paddingTop={"9rem"}
+            justifyContent={"space-around"}
+            direction={"row"}
+            width={"60%"}
+          >
+            <Button
+              size="large"
+              variant={"outlined"}
+              onClick={() => {
+                setFaceMenu(true);
+                setCtaMenu(false);
+              }}
+              sx={{ width: "100px", height: "100px", borderRadius: "50px" }}
+            >
+              No
+            </Button>
+            <Button
+              size="large"
+              variant={"contained"}
+              onClick={() => {
+                setOpen(true);
+              }}
+              sx={{ width: "100px", height: "100px", borderRadius: "50px" }}
+            >
+              Yes
+            </Button>
+          </Stack>
+        </Box>
+        <Dialog
+          open={open}
+          onClose={() => {
+            setOpen(false);
+          }}
+        >
+          <DialogTitle>Further feedback</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              By clicking ok, you acknowledge a long list of things that must be
+              GDPR compliant and all that jazz. We will not spam you, we will
+              not sell your data, we willnot do anything with your data except
+              for contacting you about your experience with us.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              id="email"
+              sx={{ marginTop: "1rem" }}
+              label="Email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              type="email"
+              fullWidth
+              variant="filled"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={() => {
+                submitEmail(email, selectedLocation);
+              }}
+            >
+              Ok
+            </Button>
+          </DialogActions>
+        </Dialog>
       </main>
     </>
   );
